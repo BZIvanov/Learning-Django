24a7b2ce24880180d8c2b4f0cdf1c020e296cc92
BZIvanov <biser.z.ivanov@gmail.com>
2024-08-18 18:07:35 +0300

    update MyBlog project with django admin

---
+++ DemoProjects/MyBlog/README.md
@@ -0,0 +1,11 @@
+# MyBlog Project
+
+## Running the project
+
+Create admin user with the following command `python manage.py createsuperuser`. Start the server with `python manage.py runserver` and visit `http://localhost:8000/admin/`.
+
+Create the needed data for the website and visit `http://localhost:8000/`.
+
+## Creating the data
+
+For the image name provide `mountains.png`, which is available in static folder.
--- DemoProjects/MyBlog/blog/views.py
+++ DemoProjects/MyBlog/blog/views.py
@@ -1,52 +1,23 @@
-from django.shortcuts import render
-from datetime import date
+from django.shortcuts import render, get_object_or_404
 
-dummy_posts = [
-    {
-        "slug": "hiking",
-        "image": "mountains.png",
-        "author": "Biser",
-        "date": date(2024, 8, 21),
-        "title": "Hiking",
-        "excerpt": "Hiking is fun",
-        "content": """
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis nobis
-          aperiam est praesentium, quos iste consequuntur omnis exercitationem quam
-          velit labore vero culpa ad mollitia? Quis architecto ipsam nemo. Odio.
-        """,
-    },
-    {
-        "slug": "programming",
-        "image": "mountains.png",
-        "author": "Biser",
-        "date": date(2024, 3, 10),
-        "title": "Programming",
-        "excerpt": "Programming is fun",
-        "content": """
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis nobis
-          aperiam est praesentium, quos iste consequuntur omnis exercitationem quam
-          velit labore vero culpa ad mollitia? Quis architecto ipsam nemo. Odio.
-        """,
-    },
-]
-
-
-def get_date(post):
-    return post["date"]
+from .models import Post
 
 
 def index(request):
-    sorted_posts = sorted(dummy_posts, key=get_date)
-    latest_posts = sorted_posts[-2:]
+    latest_posts = Post.objects.all().order_by("-date")[:3]
 
     return render(request, "blog/index.html", {"posts": latest_posts})
 
 
 def posts(request):
-    return render(request, "blog/posts.html", {"posts": dummy_posts})
+    posts = Post.objects.all().order_by("-date")
+
+    return render(request, "blog/posts.html", {"posts": posts})
 
 
 def post(request, slug):
-    selected_post = next(post for post in dummy_posts if post["slug"] == slug)
+    post = get_object_or_404(Post, slug=slug)
 
-    return render(request, "blog/post-details.html", {"post": selected_post})
+    return render(
+        request, "blog/post-details.html", {"post": post, "post_tags": post.tags.all()}
+    )
